194a48586d62086c2a7bda477b7b5b6b16df35f0
Felipe <felipe.escorel@gmail.com>
2019-09-23 19:41:05 -0300

    Tratar erro de parse na string + update styles

--- src/GasolinaOuAlcool.js
+++ src/GasolinaOuAlcool.js
@@ -6,7 +6,7 @@ import {
     Text,
     TextInput
 } from 'react-native';
-import {Button} from 'react-native-elements'
+import { Button } from 'react-native-elements'
 import { translate } from './locales'
 
 export default class GasolinaOuAlcool extends Component {
@@ -18,21 +18,27 @@ export default class GasolinaOuAlcool extends Component {
         }
     }
 
-    calculaMelhor = () => {        
-        const alerta = parseFloat(this.state.textInputValueGasolina)*0.7>parseFloat(this.state.textInputValueAlcool)?translate('etanol'):translate('gas')
+    calculaMelhor = () => {
+        this.state.textInputValueGasolina = this.state.textInputValueGasolina.replace(',','.')
+        this.state.textInputValueAlcool = this.state.textInputValueAlcool.replace(',','.')
+        const alerta = parseFloat(this.state.textInputValueGasolina) * 0.7 > parseFloat(this.state.textInputValueAlcool) ? translate('etanol') : translate('gas')
         // Alert.alert("É melhor comprar um carro elétrico")
-        Alert.alert(translate('msg_alert')+alerta)
+        Alert.alert(translate('msg_alert') + alerta)
     }
     render() {
         return (
-            <View>
-                <Text>{translate('gas')}</Text>
-                <TextInput onChangeText={textInputValueGasolina=>this.setState({textInputValueGasolina})} placeholder={translate('value_gas')} keyboardType='numeric' style={styles.textInput}></TextInput>
-                <Text>{translate('etanol')}</Text>
-                <TextInput onChangeText={textInputValueAlcool => this.setState({textInputValueAlcool})} placeholder={translate('value_etanol')} keyboardType='numeric' style={styles.textInput}></TextInput>
-                <Button title={translate('btn_calculate')} onPress={this.calculaMelhor}>
-                </Button>
-                <Text></Text>
+            <View style={styles.viewElements}>
+                <View >
+                    <Text >{translate('gas')}</Text>
+                    <TextInput onChangeText={textInputValueGasolina => this.setState({ textInputValueGasolina })} placeholder={translate('value_gas')} keyboardType='numeric' style={styles.textInput}></TextInput>
+                    <Text>{translate('etanol')}</Text>
+                    <TextInput onChangeText={textInputValueAlcool => this.setState({ textInputValueAlcool })} placeholder={translate('value_etanol')} keyboardType='numeric' style={styles.textInput}></TextInput>
+                </View>
+                <View style={styles.sectionContainer}>
+
+                    <Button title={translate('btn_calculate')} onPress={this.calculaMelhor}>
+                    </Button>
+                </View>
             </View>
         )
     }
@@ -40,11 +46,15 @@ export default class GasolinaOuAlcool extends Component {
 
 const styles = StyleSheet.create({
     textInput: {
-        backgroundColor: '#a9a9a9'
+        backgroundColor: '#a9a9a9',
+    },
+    viewElements: {
+        marginHorizontal: 25,
     },
     sectionContainer: {
         marginTop: 32,
-        paddingHorizontal: 24,
+        // paddingHorizontal: 24,
+        paddingHorizontal: 80,
     }
 })
 
