d9f97efdf17abf8762e0b05030beb3c1339f7046
Felipe <felipe.escorel@gmail.com>
2019-11-25 20:58:23 -0300

    :ghost:

--- android/app/src/main/java/com/gasolina_ou_alcool/MainActivity.java
+++ android/app/src/main/java/com/gasolina_ou_alcool/MainActivity.java
@@ -4,12 +4,12 @@ import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "gasolina_ou_alcool";
-    }
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gasolina_ou_alcool";
+  }
 }
